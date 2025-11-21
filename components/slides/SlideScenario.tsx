0e3fa2967ce06ef79c424a622d92b9fe16bac366
Blaswe02 <dblasweiler@gmail.com>
2025-11-21 15:07:07 +0100

    Add SlideScenario component for hiking scenario

---
+++ components/slides/SlideScenario.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const SlideScenario: React.FC = () => {
+  return (
+    <div>
+      <h2>Scenario</h2>
+      <p>
+        You are hiking in the forest with your classmates when suddenly you see
+        a bear on the path ahead…
+      </p>
+      <p>What do you do next?</p>
+    </div>
+  );
+};
+
+export default SlideScenario;

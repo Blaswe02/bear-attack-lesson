08535e0b859e312bde4ca40e2d37f8fa06267f85
Blaswe02 <dblasweiler@gmail.com>
2025-11-21 15:18:47 +0100

    Create SlideComprehension.tsx

---
+++ components/slides/SlideComprehension.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const SlideComprehension: React.FC = () => {
+  return (
+    <div>
+      <h2>Comprehension Check</h2>
+      <p>Answer the questions about the text to check your understanding.</p>
+      <ol>
+        <li>Why can running away from a bear be dangerous?</li>
+        <li>What should you do if a bear comes closer?</li>
+        <li>What is one thing you learned from this text?</li>
+      </ol>
+    </div>
+  );
+};
+
+export default SlideComprehension;

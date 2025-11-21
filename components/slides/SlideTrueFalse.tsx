8096baaafebe58c21b2541275fb6105cc48fed48
Blaswe02 <dblasweiler@gmail.com>
2025-11-21 15:28:47 +0100

    Add SlideTrueFalse component for true/false questions

---
+++ components/slides/SlideTrueFalse.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const SlideTrueFalse: React.FC = () => {
+  return (
+    <div>
+      <h2>True or False?</h2>
+      <p>Read each sentence and decide if it is TRUE or FALSE.</p>
+      <ol>
+        <li>You should always run away from a bear as fast as you can.</li>
+        <li>Staying calm can help you make better decisions in a bear encounter.</li>
+        <li>Making yourself look bigger can sometimes scare a bear away.</li>
+        <li>You should never leave food outside your tent in bear country.</li>
+      </ol>
+      <p>Discuss your answers with a partner.</p>
+    </div>
+  );
+};
+
+export default SlideTrueFalse;

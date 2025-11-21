574e7a495b79ea075d0d1a9e92aef8ac3983f972
Blaswe02 <dblasweiler@gmail.com>
2025-11-21 15:06:45 +0100

    Add SlideIntro component for bear attack lesson

---
+++ components/slides/SlideIntro.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SlideIntro: React.FC = () => {
+  return (
+    <div>
+      <h2>Bear Attack – Introduction</h2>
+      <p>
+        In this lesson you&apos;ll learn what to do (and what not to do) when you
+        encounter a bear in the wild.
+      </p>
+    </div>
+  );
+};
+
+export default SlideIntro;

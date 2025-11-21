f2a7ca5befbfbdfd2c2af12fad99715f642259ad
Blaswe02 <dblasweiler@gmail.com>
2025-11-21 14:33:38 +0100

    Add BearClaw component skeleton

---
+++ components/BearClaw.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const BearClaw: React.FC = () => {
+  return null; // of desnoods wat tekst:
+  // return <div>BearClaw component</div>;
+};
+
+export default BearClaw;
